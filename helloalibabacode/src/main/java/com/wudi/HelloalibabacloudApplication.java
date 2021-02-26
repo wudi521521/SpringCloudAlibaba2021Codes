de4e53c5f220c577ee96db1d0ca98021d77dd306
dillon <dillon@163.com>
2021-02-26 12:23:12 +0800

    提交阿里巴巴吧基础块

---
+++ helloalibabacode/src/main/java/com/wudi/HelloalibabacloudApplication.java
@@ -0,0 +1,13 @@
+package com.wudi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloalibabacloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloalibabacloudApplication.class, args);
+    }
+
+}
