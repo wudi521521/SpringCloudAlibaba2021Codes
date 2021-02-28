2f47eec20441c9406764ce98254a09b9c23e0b66
dillon <dillon@163.com>
2021-02-28 14:51:16 +0800

    nacos-client模块

---
+++ configclient9066/src/main/java/com/wudi/Config9066Application.java
@@ -0,0 +1,13 @@
+package com.wudi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Config9066Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Config9066Application.class, args);
+    }
+
+}
---
+++ configclient9066/src/test/java/com/wudi/Config9066ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wudi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Config9066ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
