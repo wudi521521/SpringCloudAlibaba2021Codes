34ebc12ba2b743f2c0c2fb423814a3bb098fe0ec
dillon <dillon@163.com>
2021-02-28 14:52:25 +0800

    提交阿里巴巴吧基础块

---
+++ product9098/src/main/java/com/wudi/Product9098Application.java
@@ -0,0 +1,15 @@
+package com.wudi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Product9098Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Product9098Application.class, args);
+    }
+
+}
