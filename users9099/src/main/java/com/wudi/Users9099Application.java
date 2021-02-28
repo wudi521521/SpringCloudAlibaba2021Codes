141baaeae541b109edd7fa7b7bb133c005092e47
dillon <dillon@163.com>
2021-02-28 14:54:55 +0800

    提交阿里巴巴吧基础块

---
+++ users9099/src/main/java/com/wudi/Users9099Application.java
@@ -0,0 +1,15 @@
+package com.wudi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Users9099Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Users9099Application.class, args);
+    }
+
+}
